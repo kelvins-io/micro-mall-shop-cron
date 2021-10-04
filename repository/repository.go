ded62e120dfad1a9d4ae147aee506195965e7119
kelvins-io <mybaishati@gmail.com>
2021-10-04 21:53:37 +0800

    交易订单搜索

--- repository/repository.go
+++ repository/repository.go
@@ -7,7 +7,7 @@ import (
 
 func ListShopInfo(sqlSelect string, pageSize, pageNum int) ([]mysql.ShopBusinessInfo, error) {
 	var result = make([]mysql.ShopBusinessInfo, 0)
-	err := kelvins.XORM_DBEngine.Table(mysql.TableShopBusinessInfo).Select(sqlSelect).
+	err := kelvins.XORM_DBEngine.Table(mysql.TableShopBusinessInfo).Select(sqlSelect).Desc("update_time").
 		Limit(pageSize, (pageNum-1)*pageSize).Find(&result)
 	return result, err
 }
